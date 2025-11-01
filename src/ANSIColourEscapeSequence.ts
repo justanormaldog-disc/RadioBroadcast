b5acdfb473d4965aace89e5b832ef7b959a98e1c
Jackson <jackson.wilmot@icloud.com>
2025-11-01 12:35:50 +1100

    Added JSDOC comments and save & restore

--- src/ANSIColourEscapeSequence.ts
+++ src/ANSIColourEscapeSequence.ts
@@ -7,9 +7,34 @@ export enum ColourCodes {
     GREEN = 10,
 }
 
+/**
+ * Resets all styles and modes
+ */
 export const reset = () => "\x1b[0m";
+
+/**
+ * Move cursor to begging of previous line
+ */
 export const prevline = () => "\x1b[#F";
+
+/**
+ * Erases the current line
+ */
 export const eraseline = () => "\x1b[2K";
+
+/**
+ * Saves the cursor's position.
+ * 
+ * Uses DEC standard.
+ */
+export const save = () => "\x1b7";
+
+/**
+ * Restores the cursor's position.
+ * 
+ * Uses DEC standard.
+ */
+export const restore = () => "\x1b8";
 
 export const foreground = (code: ColourCodes): string => {
     return `\x1b[38;5;${code}m`;
